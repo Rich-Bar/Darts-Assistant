2c2fd4e36a7516e074ad7046f36d5439672f8178
Mariomarco <trackmaniaplay@web.de>
2022-12-17 12:13:50 +0100

    Updated Gameboard WIP

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,5 @@
-var rndPlayerUserId = ""+ Math.random() * 0xFFFFFFFFFFFFFF;
-var rndGameId= ""+ Math.random() * 0xFFFFFFFFFFFFFF;
+var rndPlayerUserId = generateUUID();
+var rndGameId= generateUUID();
 
 window.database = {
     currentGame: null,
@@ -50,34 +50,101 @@ window.database = {
 
 const fields = [6,10,15,2,17,3,19,7,16,8,11,14,9,12,5,20,1,18,4,13];
 $(function(){
+    $.fn.pressEnter = function(fn) {  
+        return this.each(function() {  
+            $(this).bind('enterPress', fn);
+            $(this).keyup(function(e){
+                if(e.keyCode == 13){$(this).trigger("enterPress", e);}
+            })
+        });  
+     }; 
     //Load previous Games
     //window.database = JSON.parse(localStorage.get("dartAssistantDB"));
-
+    
+    //First Start of Assistant
+    if(database == null){
+        
     //Show Create Game Screen
-    if(database.currentGame == null){
+    }else if(database.currentGame == null){
 
     }
+    //Continue on Enter in Button-Input
+    $("section.game .accordion-collapse .input-group > input").pressEnter((e)=>{
+        $(e.target).attr('value', $(e.target).val());
+        let inputsLeft = $(e.target).parent().find('input[type=number]:not([value])');
+        if(inputsLeft.length)inputsLeft.first().focus();
+        else $(e.target).parent().find(">button >input").focus();
+    });
+    $("section.game .accordion-collapse .input-group > button > input").pressEnter((e)=>{
+        $(e.target).parent().click();
+    });
     //Calculate Throw 
-    $("#board").click(function(e){
-        let boardSize = $("#board img").innerWidth();
-        let center = {x: $('#board').innerWidth()/2 ,y:$('#board').innerHeight()/2};
+    //via inputs
+    $("section.game .accordion-collapse .input-group > input").on('change', (e)=>{
+        let totalPoints = 0;
+        $(e.target).parent().find("> input").each(function(){
+                totalPoints = parseInt($(this).val()||"0") + totalPoints;
+        });
+        $(e.target).parent().find("> button > input").val(totalPoints).attr('value', totalPoints).trigger("change")
+    });
+    //via board
+    $("section.game > .board img").click(function(e){
+        //Check if already thrown
+        if($("section.game > .board > span").length == 3){
+            $("section.game > .board > span").remove();
+            return;
+        }
+        let boardSize = $("section.game > .board img").innerWidth();
+        let center = {
+            x: $('section.game > .board img').innerWidth()/2 ,
+            y:$('section.game > .board img').innerHeight()/2
+        };
         let angle = (Math.atan2(center.y - e.offsetY, center.x - e.offsetX)* 180 / Math.PI + 189)%360;
         let distance = Math.sqrt(Math.pow(center.y - e.offsetY, 2) + Math.pow(center.x - e.offsetX, 2));
         
         let scored = 0;
         if(distance/(boardSize/2) < 0.032){
-            scored = 50;
+            scored = 50;                                // Bullseye
         }else if(distance/(boardSize/2) < 0.072){
-            scored = 25;
+            scored = 25;                                // Center Green
         }else if(distance/(boardSize/2) > 0.433 && distance/(boardSize/2) < 0.473){
-            scored = fields[Math.floor(angle/18)] * 3;
+            scored = fields[Math.floor(angle/18)] * 3;  // Tripples
         }else if(distance/(boardSize/2) > 0.709 && distance/(boardSize/2) < 0.753){
-            scored = fields[Math.floor(angle/18)] * 2;
-        }else if(distance/(boardSize/2) > 0.753){
+            scored = fields[Math.floor(angle/18)] * 2;  // Doubles
+        }else if(distance/(boardSize/2) > 0.753){       // Out
         }else{
-            scored = fields[Math.floor(angle/18)];
+            scored = fields[Math.floor(angle/18)];      // Normal
         }
         //console.log(angle, distance,distance/(boardSize/2), fields[Math.floor(angle/18)]);
         console.log(scored);
+        //Place Position Marker
+        let halfCrosshairSize = Math.floor(boardSize / 50);
+        $('section.game > .board').append('<span style="top: '+ (-halfCrosshairSize+e.offsetY) +'px;left: '+ (-halfCrosshairSize + e.offsetX) +'px;"></span>');
+        //Set Score preview
+
+        //Set Score in Input
+        var totalPoints = 0;
+        $("section.game .accordion-collapse.show .input-group > input[type='number']:not([value])").first().val(scored).attr('value', scored).trigger("change");
+        if($("section.game .accordion-collapse.show .input-group > input[type='number']:not([value])").length){
+            $("section.game .accordion-collapse.show .input-group > input[type='number']:not([value])").first().focus();
+        }else{
+            $("section.game .accordion-collapse.show .input-group > button > input").focus();
+        }
     });
 });
+
+function generateUUID() { // Public Domain/MIT
+    var d = new Date().getTime();//Timestamp
+    var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16;//random number between 0 and 16
+        if(d > 0){//Use timestamp until depleted
+            r = (d + r)%16 | 0;
+            d = Math.floor(d/16);
+        } else {//Use microseconds since page-load if supported
+            r = (d2 + r)%16 | 0;
+            d2 = Math.floor(d2/16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
