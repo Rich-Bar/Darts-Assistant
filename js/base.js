e6bbae79285cf966a42da545b62b2fe0392a98bd
Felix <felix.berger@esthetics-med.de>
2023-11-28 01:59:05 +0100

    base.js base reload db and initial db load.

--- js/base.js
+++ js/base.js
@@ -1,9 +1,56 @@
 $(() => {
+    function getDb(callback){
+        var open = window.indexedDB.open("DartsDatabase", 1);
+        window.players = null;
+        window.games = null;
+        window.activeGames = null;
+        open.onsuccess = function () {
+            console.log("success");
+            // Start a new transaction
+            var db = open.result;
+            var txGames = db.transaction("GamesStorage", "readwrite");
+            var txPlayers = db.transaction("PlayersStorage", "readwrite");
+            var txActiveGames = db.transaction("ActiveGamesStorage", "readwrite");
+            var gamesStore = txGames.objectStore("GamesStorage");
+            var playersStore = txPlayers.objectStore("PlayersStorage");
+            var activeGamesStore = txActiveGames("ActiveGamesStorage");
+            var getPlayers = playersStore.getAll();
+            getPlayers.onsuccess = function () {
+                console.log(getPlayers.result);
+                window.players = getPlayers.result;
+            };
+            var getGames = gamesStore.getAll()
+            getGames.onsuccess = function () {
+                window.games = getGames.result;
+                if (window.players && window.activeGames) callback();
+            };
+            var getActiveGames = activeGamesStore.getAll()
+            getActiveGames.onsuccess = function () {
+                window.activeGames = getActiveGames.result;
+                if (window.players && window.games) callback();
+            };
+            // Close the db when the transaction is done
+            txGames.oncomplete = function () {
+                db.close();
+            };
+            // Close the db when the transaction is done
+            txPlayers.oncomplete = function () {
+                db.close();
+            };
+        }
 
+    }
 
-    window.players = [{
-        playerId: 13,
-        username: "drich",
+    function dummy() {
+        setInterval(function(){
+            //console.log("push data");
+            pushData();
+        }, 15000)
+    }
+    getDb(dummy);
+   /* window.players = [{
+        playerId: 17,
+        username: "prich",
         picture: "none",
         sem: "table",
         playerGames: {}
@@ -16,7 +63,7 @@ $(() => {
     }];
 
     window.currentGame = {
-        gameId: "333",
+        gameId: "666",
         started: "27.11.2023 24:59",
         players: players,
         winner: players.at(0),
@@ -95,41 +142,43 @@ $(() => {
         defaultGameMode: "501",
         defaultSem: "total"
     };
+    */
     // set settings as cookie
     document.cookie = "settings="+JSON.stringify(settings)+" expires=Thu, 18 Dec 2024 12:00:00 UTC; path=/";
 
 
-// open (or create) db
-    var open = window.indexedDB.open("DartsDatabase", 1);
 
-// Create the schema
-    open.onupgradeneeded = function () {
-        var db = open.result;
-        var gamesStore = db.createObjectStore("GamesStorage", {keyPath: "gameId"});
-        var playersStore = db.createObjectStore("PlayersStorage", {keyPath: "playerId"})
-    };
 
-    open.onsuccess = function () {
-        // Start a new transaction
-        var db = open.result;
-        var txGames = db.transaction("GamesStorage", "readwrite");
-        var txPlayers = db.transaction("PlayersStorage", "readwrite");
-        var gamesStore = txGames.objectStore("GamesStorage");
-        var playersStore = txPlayers.objectStore("PlayersStorage");
-
-        // Add some data
-        gamesStore.put(games.at(0));
-        playersStore.put(players.at(0));
-        playersStore.put(players.at(1));
-
-        // Close the db when the transaction is done
-        txGames.oncomplete = function () {
-            db.close();
+    // Create the schema
+    function pushData(){
+            var open = window.indexedDB.open("DartsDatabase", 1);
+        open.onupgradeneeded = function () {
+            var db = open.result;
+            var gamesStore = db.createObjectStore("GamesStorage", {keyPath: "gameId"});
+            var playersStore = db.createObjectStore("PlayersStorage", {keyPath: "playerId"})
         };
-        // Close the db when the transaction is done
-        txPlayers.oncomplete = function () {
-            db.close();
-        };
+
+        open.onsuccess = function () {
+            // Start a new transaction
+            var db = open.result;
+            var txGames = db.transaction("GamesStorage", "readwrite");
+            var txPlayers = db.transaction("PlayersStorage", "readwrite");
+            var gamesStore = txGames.objectStore("GamesStorage");
+            var playersStore = txPlayers.objectStore("PlayersStorage");
+
+            // Add some data
+            games.forEach(game => gamesStore.put(game));
+            players.forEach(player => playersStore.put(player));
+
+            // Close the db when the transaction is done
+            txGames.oncomplete = function () {
+                    db.close();
+            };
+            // Close the db when the transaction is done
+            txPlayers.oncomplete = function () {
+                    db.close();
+            };
+        }
     }
 
 });
